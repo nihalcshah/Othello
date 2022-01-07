70db77f1fa40d8c541c2a47d90b4d5a62a1cd394
cjzulu <cjshah00@gmail.com>
2022-01-07 13:05:07 -0500

    Othello 4

--- othello.py
+++ othello.py
@@ -1,12 +1,12 @@
 import sys; args = sys.argv[1:]
-# args = ['44']
-# args = "..................x.o.....ooxx..xxxxxx.....ox.......o........... b2".split(" ")
-# args = "xxxxxx..xxxoo.o.xxoxooo.xxooooo.xxoooooxx.o.ooox.oo.ooox..o.xxx. x 51".split(" ")
-# args = ".x........x.ox.o...ox.o.x.oxox...oxxo.x.ooxooooo.x.x.o..x.ox.o.. o 4".split(" ")
-# args = "o.xxx.xooxxx..xooxxxxxxooxoxxxxooxoooxooooxxoooo.ooxxooxooo..ooo f1 48 b1".split(" ")
-# args = "..................OOO.....OOO.....OXO......X.................... 11 -2 2".split(" ")
+# args = ["262037423425164413243230384323394121451931514754615058596063335310_512_4_3484014_7_8_6521711_2184662295557_1_056_9152249"]
+# args = ['.'*27+"OX......XO"+'.'*27, 'x']
+# args = ["191844432634503746384133253240102416121129_517_8_3_1_0_430_2_63921132245_9422014314752585951_715544956485553576323-1606261"]
+# args = ["2620373433414246383039253245404344531352614958565960472916571817505563115451_22210_412_9_0_8_162_348_524192131_6_7231415"]
+# args = ["2620372512442430343340_43841233942495832435645526148311518191153_5_9_3515910_021_217_854636062_1164613294757_7_614555022"]
+args = ["26183734334020382532451310_2_916_611_0194241_4_34856394944_5_143244652614731_8125158211729505322233059605715_76214556354"]
 #Othello
-#Nihal Shah
+#Nihal Shah, Period 6, 2023
 
 def findsets():
     rows = [[j*8+i for i in range(8)] for j in range(8)]
@@ -92,7 +92,6 @@ def makemove(board, move, tokentoplay):
     return board
 
 def findmoves(board, tokentofindmovesplay):
-    
     moves = set()
     tokens = {*"XOox"}
     for i in range(len(board)):
@@ -122,63 +121,42 @@ def findmoves(board, tokentofindmovesplay):
                     moves.add(s[ind+k])
     return sorted(list(moves))
 
-def parseargs(args):    
-    newargs = args
-    board = ""
-    accmoves = []
-    possiblemoves = []
-    tokens = {*"XOox"}
-    digits = {*"0123456789"}
-    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    tokentoplay = ""
-    for arg in newargs:
-        if len(arg)==64:
-            board = arg.upper()
-        if arg in tokens:
-            tokentoplay = arg
-        k = {*arg}.intersection(digits)
-        if len(k)>0:
-            if arg[0].upper() in alpha:
-                accmoves.append(alpha.index(arg[0].upper())+((int(arg[1])-1)*8))
-            else:
-                if int(arg)>=0:
-                    accmoves.append(int(arg))
-
-    if not board:
-        board = '.'*27+"OX......XO"+'.'*27
-    board = board.upper()
-    if not tokentoplay:
-        xcount = board.count('X')
-        ocount = board.count('O')
-        if xcount<=ocount:
-            tokentoplay = 'X'
-            possiblemoves = findmoves(board, "O")
-            if not possiblemoves or (xcount+ocount)%2!=0:
-                tokentoplay = 'O'
-                possiblemoves = findmoves(board, "X")
-        else:
-            tokentoplay = 'O'
-            possiblemoves = findmoves(board, "X")
-            if not possiblemoves:
-                tokentoplay = 'X'
-                possiblemoves = findmoves(board, "O")
-    if not possiblemoves:
-        if tokentoplay.upper() == 'X':
-            possiblemoves = findmoves(board, 'O')
-        else:
-            possiblemoves = findmoves(board, 'X')
+def snapshot(board, tokentoplay, possiblemoves):
     display(board, possiblemoves)
     print(f'{board} {board.count("X")}/{board.count("O")}')
     if(possiblemoves):
         print(f"Possible moves for {tokentoplay.lower()}:", ", ".join([str(val) for val in possiblemoves]))
     else:
         print("No moves possible")
-    if not accmoves:
-        accmoves = possiblemoves
-    if not accmoves:
-        return
-    # makemove(board, possiblemoves[0], tokentoplay)
+def parseargs(args):
+    board = args[0]
+    if not board:
+        board = '.'*27+"OX......XO"+'.'*27
+    tokentoplay = args[1]
+    possiblemoves =[]
+    if not possiblemoves:
+        if tokentoplay.upper() == 'X':
+            possiblemoves = findmoves(board, 'O')
+        else:
+            possiblemoves = findmoves(board, 'X')
+    snapshot(board, tokentoplay, possiblemoves)
+
+def parseargs2(args):
+    board = ""
+    if not board:
+        board = '.'*27+"OX......XO"+'.'*27
+    board = board.upper()
+    tokentoplay = "x"
+    display(board, [])
+    print(f'{board} {board.count("X")}/{board.count("O")}')
+    accmoves = args[0]
+    accmoves = [int(accmoves[k:k+2].replace("_", "")) for k in range(0,len(accmoves),2) if int(accmoves[k:k+2].replace("_", ""))>=0]
+    # accmoves = [accmoves[i:i+2] for i in range(0, len(accmoves), 2) if accmoves[i:i+2]!="-1"]
     for move in accmoves:
+        # if "_" in move:
+        #     tokentoplay = ({*'XO'}-{tokentoplay.upper()}).pop()
+        #     continue
+        # move = int(move)
         board = makemove(board, move, tokentoplay)
         print(tokentoplay.lower(), "plays to", move)
         display(board, [])
@@ -196,7 +174,78 @@ def parseargs(args):
             else:
                 print("No moves possible")
                 return
-    # jkl = makemove(board, 57, tokentoplay)
-    # display(jkl, [])
-parseargs(args)
+
+
+def quickMove(board, token):
+    if not board:
+        board = '.'*27+"OX......XO"+'.'*27
+    
+    tokens = {*"XO"}
+    token2 = (tokens- {token.upper()}).pop()
+    board = board.upper()
+    possiblemoves = set(findmoves(board, token2.upper()))
+    # print("move:",possiblemoves)
+    for move in possiblemoves:
+        if move in {0, 7, 56, 63}:
+            return move
+    for move in possiblemoves:
+        # if move in {1, 6, 8, 9, 14, 15, 48, 49, 57,54 ,55, 62}:
+        if move == 1 or move == 8 or move ==9:
+            if board[0].upper() == token.upper():
+                return move
+        if move == 6 or move == 15 or move== 14:
+            if board[7].upper() == token.upper():
+                return move
+        if move == 48 or move == 57 or move == 49:
+            if board[56].upper() == token.upper():
+                return move
+        if move == 54 or move == 62 or move == 55:
+            if board[63].upper() == token.upper():
+                return move
+    cxpieces = {1, 6, 8, 9, 14, 15, 48, 49, 57,54 ,55, 62}
+    minmoves = 1000
+    minmove = 0
+    rowsstart = set(constraints[0][0])
+    rowsend = set(constraints[0][-1])
+    colsstart = set(constraints[1][0])
+    colsend = set(constraints[1][-1])
+    if (possiblemoves - cxpieces):
+        possiblemoves -= cxpieces
+    for move in possiblemoves:
+        if move in rowsstart or move in rowsend or move in colsstart or move in colsend:
+            return move
+    for move in possiblemoves:
+        b2 = makemove(board, move, token)
+        posmoves = findmoves(b2, token)
+
+        # posmoves = set(posmoves)-{1, 6, 8, 9, 14, 15, 48, 49, 57,54 ,55, 62}
+        if len(posmoves)<minmoves:
+            minmoves = len(posmoves)
+            minmove = move
+    if(minmove==8):
+        print("k")
+    return minmove
+    
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+    # drcol1 = set(constraints[1][1]) 
+    # drcol2 = set(constraints[1][-2])
+    # drrow1 = set(constraints[0][1])
+    # drrow2 = set(constraints[0][-2])
+
+    # return possiblemoves.pop()
+def main():
+    parseargs2(args)
+    # pass
+if __name__ == "__main__":
+    main()
+    # drawargs(sys.argv[1:])
+    # parseargs(args)
+
+# exit()
+# python
+# import othello
+# othello.quickMove("."*27+"ox......xo"+"."*27, "x")
+# l = quickMove("."*27+"ox......xo"+"."*27, "x")
+# print(l)
 #Nihal Shah, Period 6, 2023
